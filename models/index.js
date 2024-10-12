9af702494a6a55be08cbae9abd4debba10097c09
d.umarov <d.umarov@crowe.uz>
2024-10-12 20:47:20 +0500

    clear

--- models/index.js
+++ models/index.js
@@ -4,7 +4,6 @@ import { DataTypes } from 'sequelize';
 const Comments = sequelize.define('comments', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING },
-    img: {type: DataTypes.STRING },
     comment: { type: DataTypes.STRING },
     score: { type: DataTypes.INTEGER },
     sex: {type: DataTypes.STRING },
